1dc31c36b035cdb9ebc6ac8cbfb96a398846969b
LucaFormentin <64891799+Luca4DODO@users.noreply.github.com>
2025-03-31 12:23:13 +0200

    build: installed firebase

---
+++ src/lib/firebase/config.ts
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBLwdDDbIkNUN5VE3u1R-dR9vMl-5FzTQ4',
+  authDomain: 'la-banca-della-multa.firebaseapp.com',
+  projectId: 'la-banca-della-multa',
+  storageBucket: 'la-banca-della-multa.firebasestorage.app',
+  messagingSenderId: '581107301420',
+  appId: '1:581107301420:web:5d9138eb4ce806a972e589',
+  measurementId: 'G-GF7VRDGCPP',
+}
