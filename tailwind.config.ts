324c518b0dd1eda3516b73ddcac76621a038fc19
LucaFormentin <64891799+Luca4DODO@users.noreply.github.com>
2025-04-07 09:36:25 +0200

    style: basic styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primaryblue: "var(--primary-blue)",
+        secondaryblue: "var(--secondary-blue)",
       },
     },
   },
