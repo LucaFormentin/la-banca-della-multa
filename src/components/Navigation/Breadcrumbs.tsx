ee12dffd8ff79e3857fec7d7702489c8b66e6a52
LucaFormentin <64891799+Luca4DODO@users.noreply.github.com>
2025-05-07 16:27:49 +0200

    perf: optimized breadcrumbs team data fetch

--- src/components/Navigation/Breadcrumbs.tsx
+++ src/components/Navigation/Breadcrumbs.tsx
@@ -1,17 +1,43 @@
 'use client'
 
-import { Breadcrumbs as MuiBreadcrumbs } from '@mui/material'
+import {
+  IconClasses,
+  IconProps,
+  IconTypeMap,
+  Breadcrumbs as MuiBreadcrumbs,
+  Skeleton,
+} from '@mui/material'
 import {
   useParams,
   usePathname,
   useSelectedLayoutSegments,
 } from 'next/navigation'
 import HomeIcon from '@mui/icons-material/Home'
+import SportsBasketballRoundedIcon from '@mui/icons-material/SportsBasketballRounded'
 import { useEffect, useMemo, useState } from 'react'
 import { ITEMS } from '../Pages/TeamPage/MenuItems'
 import Link from 'next/link'
 import { type TeamT } from '@/lib/classes/Teams'
-import { api } from '@/lib/utils/api-client'
+import { emphasize, styled } from '@mui/material/styles'
+import Chip from '@mui/material/Chip'
+
+const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+  return {
+    backgroundColor: theme.palette.grey[100],
+    height: theme.spacing(3),
+    color: theme.palette.text.primary,
+    fontWeight: theme.typography.fontWeightRegular,
+    '&:hover, &:focus': {
+      backgroundColor: emphasize(theme.palette.grey[100], 0.06),
+      ...theme.applyStyles('dark', {
+        backgroundColor: emphasize(theme.palette.grey[800], 0.06),
+      }),
+    },
+    ...theme.applyStyles('dark', {
+      backgroundColor: theme.palette.grey[800],
+    }),
+  }
+}) as typeof Chip
 
 const Breadcrumbs = () => {
   const layoutSegments = useSelectedLayoutSegments()
@@ -20,13 +46,26 @@ const Breadcrumbs = () => {
   const [teamName, setTeamName] = useState<string>('')
 
   useEffect(() => {
-    if ('team_id' in params) {
-      api.get(`/teams/${params.team_id}/get-info`).then((data: any) => {
-        let teamData = data.data as TeamT
+    if (!params.team_id) return
+
+    const fetchTeamData = async () => {
+      try {
+        const res = await fetch(`/api/teams/${params.team_id}/get-info`, {
+          method: 'GET',
+        })
+
+        if (!res.ok) throw new Error('Failed to fetch team data')
+
+        const data = await res.json()
+        const teamData = data.data as TeamT
 
         setTeamName(teamData.name)
-      })
+      } catch (error) {
+        console.error('Error:', error)
+      }
     }
+
+    fetchTeamData()
   }, [params])
 
   const breadcrumbs = useMemo(() => {
@@ -37,8 +76,12 @@ const Breadcrumbs = () => {
     const baseHref = `/${params.uid}`
 
     // init breadcrumbs array with the landing page obj
-    const breadcrumbsArr: { label: string; href: string }[] = [
-      { label: 'Teams', href: `${baseHref}/landing` },
+    const breadcrumbsArr: { label: string; href: string; icon?: any }[] = [
+      {
+        label: 'Teams',
+        href: `${baseHref}/landing`,
+        icon: <HomeIcon sx={{ mr: 0.5 }} fontSize='inherit' />,
+      },
     ]
 
     segments.forEach((s) => {
@@ -50,6 +93,9 @@ const Breadcrumbs = () => {
         breadcrumbsArr.push({
           label: teamName,
           href: `${baseHref}/teams/${s}`,
+          icon: (
+            <SportsBasketballRoundedIcon sx={{ mr: 0.5 }} fontSize='inherit' />
+          ),
         })
       }
 
@@ -70,17 +116,32 @@ const Breadcrumbs = () => {
   return (
     <>
       <MuiBreadcrumbs sx={{ p: '0.5rem' }}>
-        {breadcrumbs.map((segment, index) => (
-          <Link
-            key={index}
-            className='flex items-center'
-            href={segment.href}
-            style={{ color: segment.href === pathname ? '#2196f3' : 'inherit' }}
-          >
-            <HomeIcon sx={{ mr: 0.5 }} fontSize='inherit' />
-            {segment.label}
-          </Link>
-        ))}
+        {breadcrumbs.map((segment, index) => {
+          let isActive = segment.href === pathname
+
+          return (
+            <StyledBreadcrumb
+              key={index}
+              component={Link}
+              href={segment.href}
+              label={
+                segment.label || (
+                  <Skeleton
+                    variant='text'
+                    sx={{ fontSize: '1rem' }}
+                    width={80}
+                  />
+                )
+              }
+              icon={segment.icon}
+              sx={{
+                color: isActive ? 'white' : 'inherit',
+                backgroundColor: (theme) =>
+                  isActive ? '#2196f3' : theme.palette.grey[700],
+              }}
+            />
+          )
+        })}
       </MuiBreadcrumbs>
     </>
   )
